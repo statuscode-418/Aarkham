e961cdf9e7f35fc6cda4d722e872eb9e56653cdf
NabajitBhadury <nabajitbhadury@gmail.com>
2025-06-21 03:30:33 +0530

    build issue fixed

---
+++ types/interfaces.ts
@@ -0,0 +1,3 @@
+﻿export enum ActionType { SWAP = 0, STAKE = 1, HARVEST = 2, LEND = 3, BORROW = 4, CUSTOM = 5 }
+export enum StrategyType { ARBITRAGE = 0, YIELD_FARMING = 1, LIQUIDATION = 2, REFINANCING = 3, CUSTOM = 4, FLASH_LOAN = 5 }
+export enum ExecutionStatus { PENDING = 0, EXECUTING = 1, COMPLETED = 2, FAILED = 3, CANCELLED = 4, EXPIRED = 5 }
